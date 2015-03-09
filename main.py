5ec5fd146bb6fae825755933fc3a86e4cdb9d307
roboboat <roboboat@roboboat.(none)>
2015-03-09 16:39:03 -0400

    Final ROSification for autonomous test

--- main.py
+++ main.py
@@ -15,9 +15,13 @@ Usage: Not yet determined
 from subprocess import call
 from sys import argv,exit
 from threading import Timer
+from team_software.msg import RandTheta
+
 import cv2
 import colordetection as cd
 import time
+import rospy
+
 
 def detection_loop(args):
 	for ctr,arg in enumerate(args):
@@ -30,37 +34,40 @@ def detection_loop(args):
 	return angle_out
 
 def main():
-	stdin_args = ''
-	cpp_file = "./triclops/src/examples/common/stereoto3dpoints/stereoto3dpoints"
-	reconstruct_file = "./triclops/src/examples/common/stereoto3dpoints/reconstruction.py"
-	rectified_file = 'rectified.pgm'
-	# Parse for inputs
-	if len(argv) == 2:
-		cpp_file = argv[1]
-	elif len(argv) == 3:
-		cpp_file = argv[1]
-		stdin_args = argv[2]
-		# Try to call C++ file
-		cpp_file = "./" + cpp_file
-		cpp_file = [cpp_file]
-	while 1:
-		try:
-			args = [cpp_file,reconstruct_file,rectified_file,stdin_args]
-			angle_out = detection_loop(args)  
-		except:
-			print 'Except block, default'
-			angle_out = 90
-		# After navigation, call motor code
-		# motor_code(angle_out)
-		print 'before sleep'
-		time.sleep(10)
-		print 'after sleep'
-		# Call motor code to drive forward
-		# motor_code(0)
-		# Rerun detection loop
+    # Ros publisher stuff
+    pub = rospy.Publisher('RandTheta', RandTheta, queue_size=1)
+    rospy.init_node('nav_node')
+    payload = RandTheta(); payload.theta = 0; payload.r = 0
+    rate = rospy.Rate(10)
+    # Not ROS publisher stuff
+    stdin_args = ''
+    cpp_file = "./triclops/src/examples/common/stereoto3dpoints/stereoto3dpoints"
+    reconstruct_file = "./triclops/src/examples/common/stereoto3dpoints/reconstruction.py"
+    rectified_file = 'rectified.pgm'
+    # Parse for inputs
+    if len(argv) == 2:
+	cpp_file = argv[1]
+    elif len(argv) == 3:
+	cpp_file = argv[1]
+	stdin_args = argv[2]
+	# Try to call C++ file
+	cpp_file = "./" + cpp_file
+	cpp_file = [cpp_file]
+    while not rospy.is_shutdown():
+	try:
+	    args = [cpp_file,reconstruct_file,rectified_file,stdin_args]
+	    payload.theta = detection_loop(args)  
+	except:
+	    print 'Except block, default'
+	    payload.theta = 90.0
+	# Publish this shit yo
+	pub.publish(payload)
+	rate.sleep()
 	
 # Boiler plate code
 if __name__ == '__main__':
-    main()
-
+    try: 
+	main()
+    except rospy.ROSInterruptException:
+	pass
 
--- src/basic_control_node.py
+++ src/basic_control_node.py
@@ -8,55 +8,63 @@
 import rospy
 from team_software.msg import MotorPower
 from team_software.msg import RandTheta
+from team_software.msg import Stop
 
+# Global stopvar
+fuck_the_police = False
 
-def power_level(data,motors):
+def power_level(data,motor_power):
   '''
   Function to send power levels to motor 
   '''
-  motors.set_thrust(data.power1,data.power2)
-  if motors.send_motors_power_level():
-    motor_response_to_ros(motors)
-  
 
-def motor_response_to_ros(motors):
+  if data.theta < 0: 
+    motor_power.power1= 0.1
+    motor_power.power2=-0.1
+  elif data.theta > 0:
+    motor_power.power1 = -0.1
+    motor_power.power2 =  0.1
+  elif data.theta == 0:
+    motor_power.power1 = 0.1
+    motor_power.power2 = 0.1
+  else: 
+    motor_power.power1 = 0
+    motor_power.power2 = 0
+
+def check_stop(data):
+  # Set global for modify purposes
+  global fuck_the_police
+  fuck_the_police = data.stop
   '''
-  Function to take motor response and put it on ROS
-  '''
-  motor_data = MotorResponse()
-
-  #filling out MotorResponse message
-  motor_data.header.stamp.secs = motors.now.secs
-  motor_data.header.stamp.nsecs = motors.now.nsecs
-  motor_data.motor_id = motors.response[1]
-  motor_data.rpm = motors.response[7]
-  motor_data.bus_voltage = motors.response[8]
-  motor_data.bus_current = motors.response[9]
-  motor_data.temperature = motors.response[10]
-  motor_data.fault_flag = motors.response[11]
-  motors.toggle_node_id()  
-
-  motors.pub.publish(motor_data)    
-  
-  
+  if (data.stop == 0.0):
+    fuck_the_police = False
+  else: 
+    fuck_the_police = True
+  '''  
 def motor_node():
   '''
   Top level function to handle connection of motors with ROS
   '''
-  motors = motor_comm()
+  pub = rospy.Publisher('motor_power', MotorPower, queue_size=10)
+  rospy.init_node('control_node')
+  rate = rospy.Rate(10)
+  motor_power=MotorPower()
 
-  motors.pub = rospy.Publisher('motor_data', MotorResponse, queue_size=10)
-  rospy.init_node('motor_comm')
-  rate = rospy.Rate(5)
-  motors.now = rospy.get_rostime()
-  
-  #spins at rate and puts the motors response on ROS
+  motor_power.power1=0.0
+  motor_power.power2=0.0
+
+  # spins at rate and puts the motors response on ROS
+  # send true on Stop topic to stop motors
   while not rospy.is_shutdown():
-    if motors.send_motors_power_level():
-      motor_response_to_ros(motors)
-    rospy.Subscriber("motor_power", MotorPower, power_level, motors)
+    if (fuck_the_police):
+        motor_power.power1=0.0
+        motor_power.power2=0.0
+        pub.publish(motor_power)
+    else:
+      pub.publish(motor_power)
+    rospy.Subscriber("RandTheta", RandTheta, power_level, motor_power)
+    rospy.Subscriber("Stop", Stop, check_stop)
     rate.sleep()
-    
 
 
 if __name__ == '__main__':
