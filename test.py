0d7eec7f22521e8976e6a02e3cdc0bd466d2c351
roboboat <roboboat@roboboat.(none)>
2015-04-07 16:36:57 -0400

    Changed camera number to work with webcame

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import cv2
 
 from cv2 import *
 # initialize the camera
-cam = VideoCapture(0)   # 0 -> index of camera
+cam = VideoCapture(1)   # 0 -> index of camera
 s, img = cam.read()
 if s:    # frame captured without any errors
     #namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
