b5c2f49944646f20024d3ada475d7dd1e893fa91
JS <tud45372@temple.edu>
2015-04-18 15:59:11 -0400

    Changed rpm message so that it is rpm0 and rpm1. Changed basic_control_node to reflect this change

--- src/basic_control_node.py
+++ src/basic_control_node.py
@@ -19,22 +19,18 @@ def set_rpm(data,motor_rpm,rpm_scalar):
   '''
 
   if data.theta < 0: 
-    motor_rpm.rpm1= 1000
-    motor_rpm.rpm2=-1000
+    motor_rpm.rpm0= rpm_scalar*1000*data.r
+    motor_rpm.rpm1= -rpm_scalar*1000*data.r
   elif data.theta > 0:
-    motor_rpm.rpm1 = -1000
-    motor_rpm.rpm2 =  1000
+    motor_rpm.rpm0 = -rpm_scalar*1000*data.r
+    motor_rpm.rpm1 =  rpm_scalar*1000*data.r
   elif data.theta == 0:
-    motor_rpm.rpm1 = 1000
-    motor_rpm.rpm2 = 1000
+    motor_rpm.rpm0 = rpm_scalar*1000*data.r
+    motor_rpm.rpm1 = rpm_scalar*1000*data.r
   else: 
+    motor_rpm.rpm0 = 0
     motor_rpm.rpm1 = 0
-    motor_rpm.rpm2 = 0
 
-def polar_to_cart():
-  '''
-  Function to take an R and Theta and convert it to X and
-  '''
     
 def check_stop(data):
   # Set global for modify purposes
@@ -55,16 +51,16 @@ def motor_node():
   rate = rospy.Rate(10)
   motor_rpm=MotorRPM()
 
+  motor_rpm.rpm0=0.0
   motor_rpm.rpm1=0.0
-  motor_rpm.rpm2=0.0
   rpm_scalar=1
   
   # spins at rate and puts the motors response on ROS
   # send true on Stop topic to stop motors
   while not rospy.is_shutdown():
     if (fuck_the_police):
+        motor_rpm.rpm0=0.0
         motor_rpm.rpm1=0.0
-        motor_rpm.rpm2=0.0
         pub.publish(motor_rpm)
     else:
       pub.publish(motor_rpm)
