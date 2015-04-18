f90ae3ad9bec58a98f29f31af1da81e8301f3cca
Zack_Smith <zack.smith@temple.edu>
2015-04-18 19:10:14 -0400

    Fixed psuedosrv.py's responses.

--- heartbeat/test_srv/pseudo_server.py
+++ heartbeat/test_srv/pseudo_server.py
@@ -1,10 +1,10 @@
-from flask import * #Flask, render_template, request, url_for
+from flask import * 
 
 app = Flask(__name__)
 
 @app.route('/')
 def form():
-    return 'TEST '
+    return 'Psuedo Srv Landing Page'
 
 #6.2 HEARTBEAT RESPONSE
 @app.route('/heartbeat/<course>/<teamCode>', methods=['GET','POST'])
@@ -12,28 +12,45 @@ def heartbeat(course, teamCode):
     response = jsonify(success = True)
     return response 
 
-#5.5 UAV
-@app.route('/interop/report/<course>/<teamCode>', methods=['GET','POST'])
+# 5.5 UAV
+# Upload Image
+# Request - Multipart/mixed
+@app.route('/interop/image/<course>/<teamCode>', methods=['GET','POST'])
 def uav(course, teamCode):
+    response = jsonify(id='yoloswagupload420')
+    return response 
+
+# Reporting shape 
+# Request - course:<course> team:<teamcode> shape: <shape> image: <imageID>
+@app.route('/interop/report/<course>/<teamCode>', methods=['GET','POST'])
+def uav_report(course, teamCode):
     response = jsonify(success = True)
     return response 
 
+
 #5.4 PINGER
+# Report Pinger
+# Request - course: <course> team: <teamcode> buoycolor: buoycolor
 @app.route('/pinger/<course>/<teamCode>', methods=['GET','POST'])
 def pinger(course, teamCode):
     response = jsonify(success = True)
     return response 
 
 #5.3 AUTOMATED DOCKING
+# For grabbing sequence info etc
+# Request - GET on URL
 @app.route('/automatedDocking/<course>/<teamCode>', methods=['GET','POST'])
 def docking(course, teamCode):
-    response = jsonify(success = True, dockingBaySequence = True )
+    res = [ {'symbol':'triangle','color':'blue'},   {'symbol':'square','color':'red'}] 
+    response = jsonify(dockingBaySequence = res)
     return response 
 
 #5.2 OBSTACLE AVOIDANCE
+# For getting starting gate info etc
+# Request - GET on URL
 @app.route('/obstacleAvoidance/<course>/<teamCode>', methods=['GET','POST'])
 def obstacle(course, teamCode):
-    response = jsonify(success = True, gateCode = ['1','Z'])
+    response = jsonify(gateCode = ['1','Z'])
     return response 
 
 if __name__ == '__main__':
